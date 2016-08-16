43c23a8a148458514bb7d51882b3e20aabbfd719
Huachao Zhang <chaoye917@gmail.com>
2016-08-16 15:48:11 -0700

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,15 +3,15 @@ import firebase from 'firebase';
 try {
   // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyB3fGTiE0_JQYNPUpZkWLY3MNHHxOAqf30",
-    authDomain: "huachao-todo-app.firebaseapp.com",
-    databaseURL: "https://huachao-todo-app.firebaseio.com",
-    storageBucket: "huachao-todo-app.appspot.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTO_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
 
   firebase.initializeApp(config);
 } catch (e) {
-  
+
 }
 
 export const firebaseRef = firebase.database().ref();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 var webpack = require('webpack');
 var path = require('path');
+var envFile = require('node-env-file');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+try {
+  // load env
+  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+} catch (e) {
+
+}
 
 module.exports = {
   entry: [
@@ -20,6 +28,15 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET)
       }
     })
   ],
