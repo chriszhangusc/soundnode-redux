a4d05edc99db4804d240581f992674ec229b82d9
Huachao Zhang <chaoye917@gmail.com>
2016-09-11 17:39:36 -0700

    Extract playlist reducer from playlists reducer

---
+++ scripts/reducers/playlist.js
@@ -0,0 +1,36 @@
+import * as ActionTypes from '../constants/ActionTypes';
+
+const PLAYLIST_INITIAL_STATE = {
+  isFetching: false,
+  songs: {},
+  songIds: [],
+  nextUrl: null,
+};
+
+const playlist = (state = PLAYLIST_INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ActionTypes.REQUEST_SONGS:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case ActionTypes.RECEIVE_SONGS:
+      return {
+        ...state,
+        isFetching: false,
+        nextUrl: action.nextUrl,
+        songIds: [...state.songIds, ...action.songIds],
+        songs: {...state.songs, ...action.songs}
+      };
+    default:
+      return state;
+  }
+};
+
+/* Playlist Selectors */
+// All states in func params are refered to as single playlist
+export const getSongIds = state => state.songIds
+export const getSongsObj = state => state.songs
+export const getIsFetching = state => state.isFetching
+
+export default playlist;
--- scripts/reducers/playlists.js
+++ scripts/reducers/playlists.js
@@ -1,32 +1,6 @@
 import * as ActionTypes from '../constants/ActionTypes';
-
+import playlist, { getSongsObj, getSongIds, getIsFetching } from './playlist';
 /* Reducers */
-const PLAYLIST_INITIAL_STATE = {
-  isFetching: false,
-  songs: {},
-  songIds: [],
-  nextUrl: null,
-};
-
-const playlist = (state = PLAYLIST_INITIAL_STATE, action) => {
-  switch (action.type) {
-    case ActionTypes.REQUEST_SONGS:
-      return {
-        ...state,
-        isFetching: true,
-      };
-    case ActionTypes.RECEIVE_SONGS:
-      return {
-        ...state,
-        isFetching: false,
-        nextUrl: action.nextUrl,
-        songIds: [...state.songIds, ...action.songIds],
-        songs: {...state.songs, ...action.songs}
-      };
-    default:
-      return state;
-  }
-};
 
 const PLAYLISTS_INITIAL_STATE = {};
 
@@ -46,11 +20,7 @@ const playlists = (state = PLAYLISTS_INITIAL_STATE, action) => {
       return state;
   }
 }
-/* Playlist Selectors */
-// All states in func params are refered to as single playlist
-export const getSongIds = state => state.songIds
-export const getSongsObj = state => state.songs
-export const getIsFetching = state => state.isFetching
+
 /* Playlists Selectors */
 // All states in func params are refered to as playlists
 export const getSongByIdAndPlaylist = (state, songId, playlist) => {
