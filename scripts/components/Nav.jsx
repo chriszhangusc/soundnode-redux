e19dc0291b912a62ef11ac6399b97b7d07512594
Huachao Zhang <chaoye917@gmail.com>
2016-09-25 11:46:53 -0700

    Change navbar to bootstrap

--- scripts/components/Nav.jsx
+++ scripts/components/Nav.jsx
@@ -13,18 +13,28 @@ function renderSearchField() {
 
 export default function Nav() {
   return (
-    <div className="nav">
-      <div className="container clearfix">
-        <div className="nav-logo">
-          <i className="icon ion-radio-waves" />
+    <nav className="navbar navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="http://localhost:3000">
+            <span className="glyphicon glyphicon-headphones" id="nav-icon" /><span>ReduxSound</span>
+          </a>
         </div>
-        <div className="nav-nav float-left">
-          <div className="nav-nav-item">
-            <a className="nav-nav-item-link active" href="/">SoundRedux</a>
-          </div>
-        </div>
-        { renderSearchField() }
       </div>
-    </div>
+    </nav>
   );
 }
+
+// <div className="nav">
+//   <div className="container clearfix">
+//     <div className="nav-logo">
+//       <i className="icon ion-radio-waves" />
+//     </div>
+//     <div className="nav-nav float-left">
+//       <div className="nav-nav-item">
+//         <a className="nav-nav-item-link active" href="/">SoundRedux</a>
+//       </div>
+//     </div>
+//     { renderSearchField() }
+//   </div>
+// </div>
