ec580e0efaab7764350a1cc4a93b957863bf63ed
Huachao Zhang <chaoye917@gmail.com>
2016-09-25 11:07:01 -0700

    Change theme to dark mode

--- scripts/components/Main.jsx
+++ scripts/components/Main.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import Toolbar from './Toolbar';
 import SongCardListContainer from '../containers/SongCardListContainer';
 import PlayerContainer from '../containers/Player/PlayerContainer';
-
+import Sidebar from './Sidebar';
 export default function Main() {
   return (
-    <div className="songs">
-      <Toolbar />
-      <SongCardListContainer />
-      <PlayerContainer />
+    <div>
+      <Sidebar />
+      <div className="content">
+        <SongCardListContainer />
+        <PlayerContainer />
+      </div>
     </div>
   );
 }
+// export default function Main() {
+//   return (
+//     <div className="songs">
+//       <Toolbar />
+//       <Sidebar />
+//       <SongCardListContainer />
+//       <PlayerContainer />
+//     </div>
+//   );
+// }
---
+++ scripts/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Sidebar = () => (
+  <div id="wrapper">
+    <div id="sidebar-wrapper">
+      <ul className="sidebar-nav">
+        <li>
+          <a href="#">Top 50</a>
+        </li>
+        <li>
+          <a href="#">Stream</a>
+        </li>
+        <li>
+          <a href="#">Likes</a>
+        </li>
+        <li>
+          <a href="#">Tracks</a>
+        </li>
+        <li>
+          <a href="#">Playlists</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default Sidebar;
