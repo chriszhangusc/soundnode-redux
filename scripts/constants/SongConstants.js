0a770c76c40db15cdf46a72adec7efa817e89251
Huachao Zhang <chaoye917@gmail.com>
2016-09-26 23:58:43 -0700

    Add fetch backend server start script

--- scripts/constants/SongConstants.js
+++ scripts/constants/SongConstants.js
@@ -1,5 +1,3 @@
-import { CLIENT_ID } from './Config';
-
 export const GENRES = [
   'All-Music',
   'Ambient',
@@ -12,13 +10,5 @@ export const GENRES = [
   'Pop',
   'R&B & Soul'
 ];
-
-// export const SEED_FETCH_URL =
-// `https://api.soundcloud.com/tracks?linked_partitioning=1&client_id=${CLIENT_ID}&offset=0&limit=50`;
-
-// export const TOP_FETCH_URL = `https://api.soundcloud.com/charts?kind=top&genre=soundcloud%3Agenres%3Acountry&client_id=${CLIENT_ID}&limit=20&offset=0&linked_partitioning=1`;
-// export const TOP_FETCH_URL = 'https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud%3Agenres%3Aall-music&client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea&limit=20&offset=0&linked_partitioning=1&app_version=1474905173';
-
-
 
 export const DEFAULT_GENRE = 'All-Music';
