bcfd98a70ed98299f45a52f29d0dd78b59e4075a
Huachao Zhang <chaoye917@gmail.com>
2016-10-12 14:46:15 -0700

    Add title for search results page

--- scripts/routes/search/components/SearchResults.jsx
+++ scripts/routes/search/components/SearchResults.jsx
@@ -1,10 +1,19 @@
 import React, { PropTypes } from 'react';
 import SearchResultListContainer from '../containers/SearchResultListContainer';
 
-const SearchResults = () => {
+const SearchResults = ({ location: { query } }) => {
+  const keyword = query.q;
+  const title = `Results for: ${keyword}`;
   return (
-    <SearchResultListContainer />
+    <div className="container">
+      <h1 className="search-results-title">{title}</h1>
+      <SearchResultListContainer />
+    </div>
   );
+};
+
+SearchResults.propTypes = {
+  location: PropTypes.object
 };
 
 export default SearchResults;
