31e55e40811a62037a88022d64c2b92e2602db9d
chao <chaoye917@gmail.com>
2016-10-29 20:24:40 -0700

    Change project name

--- client/components/Nav/components/Nav.jsx
+++ client/components/Nav/components/Nav.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import NavSearchContainer from '../containers/NavSearchContainer';
 import NavUserContainer from '../containers/NavUserContainer';
 /* This is our main Nav layout */
-const Nav = () => {
-  return (
-    <nav className="navbar navbar-fixed-top">
-      <div className="container">
-        <div className="navbar-header">
-          <a className="navbar-brand" href="http://localhost:3000">
-            <span className="glyphicon glyphicon-headphones" id="nav-icon" /><span>ReduxSound</span>
-          </a>
-        </div>
-        <div className="nav table-display pull-right">
-          <NavSearchContainer />
-          <NavUserContainer />
-        </div>
+const Nav = () => (
+  <nav className="navbar navbar-fixed-top">
+    <div className="container">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="http://localhost:3000">
+          <span className="glyphicon glyphicon-headphones" id="nav-icon" /><span>Redux Music</span>
+        </a>
       </div>
-    </nav>
-  );
-};
+      <div className="nav table-display pull-right">
+        <NavSearchContainer />
+        <NavUserContainer />
+      </div>
+    </div>
+  </nav>
+);
 
 export default Nav;
