23ee2e8433908fbe337036dbf3b0c6b4ba24cc80
chao <chaoye917@gmail.com>
2017-03-20 11:05:54 -0700

    Change node.js' ES6 into using babel-node

--- client/components/SongCardList/SongCardList.jsx
+++ client/components/SongCardList/SongCardList.jsx
@@ -5,12 +5,13 @@ import infiniteScroll from 'client/components/hocs/InfiniteScroll';
 import { List } from 'immutable';
 
 const renderSongCardList = (trackIds) => {
-  // One row is fine
+  // We are using index + trackId as the key because sometimes it will have flattenChildren warning
+  // issue when using replace to change url in router index.jsx
   return (
     <div className="row">
       {
         trackIds.toArray().map((trackId, index) => (
-          <div className="col-sm-6 col-md-4 col-lg-3" key={index}>
+          <div className="col-sm-6 col-md-4 col-lg-3" key={index + trackId}>
             <SongCardContainer trackId={trackId} trackIds={trackIds} />
           </div>
         ))
--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,2 @@
 // Inorder to use ES6 on the server.
-require('babel-register');
 require('./api');
