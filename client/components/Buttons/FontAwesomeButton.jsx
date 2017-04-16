8eeb90c1423fde688879e199e3e37558715e2a96
chao <chaoye917@gmail.com>
2017-04-16 09:03:59 -0700

    Refactor FontAwesomeButton to use styled components and it is amazing

--- client/components/Buttons/FontAwesomeButton.jsx
+++ client/components/Buttons/FontAwesomeButton.jsx
@@ -2,17 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { defaultEventHandlerFactory } from 'client/utils/FactoryUtils';
 import FontAwesome from 'react-fontawesome';
-import styles from './IconButton.css';
+// import styles from './IconButton.css';
+import styled from 'styled-components';
+import { FONT_COLOR_PRIMARY, FONT_COLOR_SECONDARY, THEME_COLOR } from 'client/constants/Colors';
+// .icon-button {
+//   color: font-color-primary;
+//   padding: 4px;
+//   border: none;
+//   outline: none;
+//   background-color: transparent;
+//   cursor: pointer;
+//   margin: 0 auto;
+//   text-align: center;
+// }
+
+// .icon-button-active {
+//   composes: icon-button;
+//   color: theme-color;
+// }
+
+// .icon-button:hover {
+//   color: font-color-secondary;
+// }
+
+const OuterButton = styled.button`
+  color: ${props => props.active ? THEME_COLOR : FONT_COLOR_SECONDARY};
+  padding: 4px;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+  margin: 0 auto;
+  text-align: center;
+
+  &:hover {
+    color: ${FONT_COLOR_PRIMARY}
+  }
+`;
+
+// {/*<button
+//       className={active ? styles.iconButtonActive : styles.iconButton}
+//       title={title}
+//       onClick={onClick}
+//     >
+//       <FontAwesome name={name} />
+//     </button>*/}
 
 function FontAwesomeButton({ title, name, active, onClick }) {
   return (
-    <button
-      className={active ? styles.iconButtonActive : styles.iconButton}
-      title={title}
-      onClick={onClick}
-    >
+    <OuterButton title={title} onClick={onClick}>
       <FontAwesome name={name} />
-    </button>
+    </OuterButton>
   );
 }
 
---
+++ client/constants/Colors.js
@@ -0,0 +1,20 @@
+export const BLACK = '#0f1010';
+export const LIGHT_BLACK = '#1d1f21';
+export const GRAY = '#949599';
+export const ORANGE = '$f50';
+export const LIGHTER_GRAY = '#ededed';
+
+
+// @value light-black: #1d1f21;
+// @value lighter-black: #242728;
+// @value dark-gray: #4f4f51;
+// @value gray: #949599;
+// @value light-gray: #CCC;
+// @value semi-transparent-light: rgba(0, 0, 0, 0.22);
+// @value semi-transparent-dark: rgba(0, 0, 0, 0.7);
+// @value lighter-gray: #ededed;
+// @value orange: #f50;
+
+export const FONT_COLOR_PRIMARY = LIGHTER_GRAY;
+export const FONT_COLOR_SECONDARY = GRAY;
+export const THEME_COLOR = ORANGE;
