22e3aaff4e8f898bbc464519707b06c83e75796a
chao <chaoye917@gmail.com>
2017-05-01 09:46:53 -0700

    Adjust sidebar width in 4k screens

--- client/css/variables.js
+++ client/css/variables.js
@@ -4,7 +4,7 @@ export const NAV_BAR_HEIGHT = 70;
 export const SIDEBAR_ITEM_HEIGHT = '80px';
 export const SIDEBAR_WIDTH_DESKTOP = '210px';
 export const SIDEBAR_WIDTH_DESKTOP_LG = '230px';
-export const SIDEBAR_WIDTH_4K = '250px';
+export const SIDEBAR_WIDTH_4K = '280px';
 
 export const PLAYLIST_WIDTH_DESKTOP = '300px';
 export const PLAYLIST_WIDTH_DESKTOP_LG = '400px';
