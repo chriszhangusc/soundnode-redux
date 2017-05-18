90c437e77c0bcd4a84c8ee58b23b1ca1e62a889f
MiniPekka <chaoye917@gmail.com>
2017-05-17 17:37:37 -0700

    Implement fetch user in auth

--- client/features/auth/authActions.js
+++ client/features/auth/authActions.js
@@ -1,12 +1,20 @@
 import SC from 'soundcloud';
 import { CLIENT_ID, REDIRECT_URI } from 'client/common/constants/authConsts';
 
+export function fetchMe(session) {
+  fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
 export function login() {
-  return dispatch => {
+  return (dispatch) => {
     console.log('Login');
     SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
-    SC.connect().then(session => {
-      console.log(session);
+    SC.connect().then((session) => {
+      dispatch(fetchMe(session));
       // Cookies.set(OAUTH_TOKEN, session.oauth_token);
       // dispatch(setSession(session));
       // dispatch(fetchUser());
@@ -17,3 +25,5 @@ export function login() {
 export function logout() {
   console.log('Logout');
 }
+
+
