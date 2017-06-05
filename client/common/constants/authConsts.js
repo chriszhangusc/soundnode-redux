f1241c80386be8714267c988d8d55b4f4f643906
chao <chaoye917@gmail.com>
2017-06-05 11:10:11 -0700

    Fix case

--- client/common/constants/authConsts.js
+++
@@ -1,6 +0,0 @@
-// export const CLIENT_ID = 'f9e1e2232182a46705c880554a1011af'; // Sound Cloud ClientID
-
-export const CLIENT_ID = 'hV0x3cye6r1htAwy737V22PTsfd7HtOh';
-
-// export const REDIRECT_URI = `${window.location.protocol}//${window.location/callback`;
-export const REDIRECT_URI = `${window.location.protocol}//127.0.0.1:3000/callback`;
