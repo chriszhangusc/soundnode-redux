f77c2e4452abb48836493dcd134f7e6a003769fa
Chris Zhang <chaoye917@gmail.com>
2017-06-24 19:46:04 -0700

    Extract api constants

--- client/common/utils/apiUtils.js
+++ client/common/utils/apiUtils.js
@@ -4,23 +4,21 @@ import { API_PROXY_ORIGIN } from 'common/constants/appConsts';
 import { CLIENT_ID } from 'common/constants/authConsts';
 import { getOAuthToken } from 'features/auth/authUtils';
 import { SC_API_V1, SC_API_V2 } from 'common/constants/apiConsts';
-
 import { setUrlParams } from './urlUtils';
 
-
-function getSCV2ProxyOrigin() {
+export function getSCV2ProxyOrigin() {
   return `${API_PROXY_ORIGIN}/sc/v2`;
 }
 
-function getSCV1ProxyOrigin() {
+export function getSCV1ProxyOrigin() {
   return `${API_PROXY_ORIGIN}/sc/v1`;
 }
 
-function isSCV1Request(requestUrl) {
+export function isSCV1Request(requestUrl) {
   return requestUrl && requestUrl.startsWith(SC_API_V1);
 }
 
-function isSCV2Request(requestUrl) {
+export function isSCV2Request(requestUrl) {
   return requestUrl && requestUrl.startsWith(SC_API_V2);
 }
 
@@ -39,11 +37,14 @@ export function getProxyRequest(requestUrl) {
   return requestUrl;
 }
 
-/**
- * Response success handler
- * @param  {object} response - response object from fetch web api
- * @returns {Promise}         - response json wrapped in Promise
- */
+// If we have authed already, use oauth_token, otherwise use client_id
+export function appendTokenToUrl(requestUrl) {
+  const token = getOAuthToken();
+  return token
+    ? setUrlParams(requestUrl, { oauth_token: token })
+    : setUrlParams(requestUrl, { client_id: CLIENT_ID });
+}
+
 export function checkStatus(response) {
   if (!response.ok) {
     const error = new Error(response.statusText);
@@ -58,22 +59,9 @@ export function parseJson(response) {
   return response.json().then(json => camelizeKeys(json));
 }
 
-// If we have authed already, use oauth_token, otherwise use client_id
-export function appendTokenToUrl(fetchUrl) {
-  const token = getOAuthToken();
-  return token
-    ? setUrlParams(fetchUrl, { oauth_token: token })
-    : setUrlParams(fetchUrl, { client_id: CLIENT_ID });
-}
-
 // Check if the fetchUrl is v1 or v2 and convert them to point to our proxy server accordingly
 export function makeRequest(requestUrl, fetchOptions) {
   const proxyRequest = getProxyRequest(requestUrl);
   const finalUrl = appendTokenToUrl(proxyRequest);
-  return (
-    fetch(finalUrl, fetchOptions)
-      .then(checkStatus)
-      .then(parseJson)
-  );
+  return fetch(finalUrl, fetchOptions).then(checkStatus).then(parseJson);
 }
-
--- client/features/charts/chartsApi.js
+++ client/features/charts/chartsApi.js
@@ -1,9 +1,9 @@
 import { trackArraySchema } from 'app/schema';
 import { makeRequest } from 'common/utils/apiUtils';
 import { normalizeResponse } from 'common/utils/normalizeUtils';
+import { SC_API_V2 } from 'common/constants/apiConsts';
 
 // https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud%3Agenres%3Aall-music&linked_partitioning=1&limit=25&offset=0&client_id=f9e1e2232182a46705c880554a1011af
-
 function transform(response) {
   return {
     ...response,
@@ -12,13 +12,10 @@ function transform(response) {
 }
 
 export function fetchCharts(genre, limit = 20) {
-  // const fetchUrl = constructFetchUrl(baseUrl, endpoint, queryParams);
-  const requestUrl = `https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud:genres:${genre}&linked_partitioning=1&limit=${limit}&offset=0`;
+  const requestUrl = `${SC_API_V2}/charts?kind=top&genre=soundcloud:genres:${genre}&linked_partitioning=1&limit=${limit}&offset=0`;
   return (
-    // makeRequest(initialFetchUrl)
     makeRequest(requestUrl)
       // transform collection: [{score, track}] to collection: [track]
-      // .then(response => transform(response))
       .then(transform)
       .then(transformed => normalizeResponse(transformed, trackArraySchema))
   );
