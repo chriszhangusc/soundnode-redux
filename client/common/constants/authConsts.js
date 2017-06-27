d6a23b3887ae7dfac51b124c17cd7393fd9c4e6e
Chris Zhang <chaoye917@gmail.com>
2017-06-26 18:41:40 -0700

    Updated soundcloud callback uri and clientid

--- client/common/constants/authConsts.js
+++ client/common/constants/authConsts.js
@@ -1,6 +1,11 @@
 // export const CLIENT_ID = 'f9e1e2232182a46705c880554a1011af'; // Sound Cloud ClientID
-import { HOST } from './appConsts';
+import { HOST, isDev } from './appConsts';
 
-export const CLIENT_ID = 'hV0x3cye6r1htAwy737V22PTsfd7HtOh';
+export const CLIENT_ID = isDev
+  ? 'ZsXqrJ5fvCT0kw8AbbJARqxuRVYqAVEX'
+  : 'WHbr8ZvTg1c0Qjga7fdRjtfiFfvLPj1e';
+
+console.log(CLIENT_ID);
 
 export const REDIRECT_URI = `${HOST}/callback`;
+console.log(REDIRECT_URI);
