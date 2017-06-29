f3d431644237b96c530ec68c18d8d9c07c5f53aa
Chris Zhang <chaoye917@gmail.com>
2017-06-28 18:19:29 -0700

    Add playlist schema

--- client/app/schema/index.js
+++ client/app/schema/index.js
@@ -4,6 +4,7 @@ import { Schema, arrayOf } from 'normalizr';
 export const userSchema = new Schema('users');
 export const commentSchema = new Schema('comments');
 export const trackSchema = new Schema('tracks');
+export const playlistSchema = new Schema('playlists');
 
 // Removing it because the user information sent back along with tracks is not intact.
 trackSchema.define({
@@ -18,3 +19,10 @@ commentSchema.define({
 export const trackArraySchema = arrayOf(trackSchema);
 export const userArraySchema = arrayOf(userSchema);
 export const commentArraySchema = arrayOf(commentSchema);
+
+playlistSchema.define({
+  user: userSchema,
+  tracks: trackArraySchema,
+});
+
+export const playlistArraySchema = arrayOf(playlistSchema);
---
+++ client/features/userPlaylists/userPlaylistsApi.js
@@ -0,0 +1,17 @@
+import { makeRequest } from 'common/utils/apiUtils';
+import { normalizeResponse } from 'common/utils/normalizeUtils';
+import { SC_API_V1 } from 'common/constants/apiConsts';
+import { playlistArraySchema } from 'app/schema';
+
+// function transform(response) {
+//   return {
+//     ...response,
+//     collection: response.collection.map(item => item.track),
+//   };
+// }
+
+// https://api.soundcloud.com/me/playlists?limit=10&format=json&oauth_token=1-283018-250047142-67772dd734f9f
+export function fetchMyPlaylists() {
+  const requestUrl = `${SC_API_V1}/me/playlists?format=json`;
+  makeRequest(requestUrl).then(response => normalizeResponse(response, playlistArraySchema));
+}
