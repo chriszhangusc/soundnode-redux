730b39e9c85ce24793e6c195e79cac98604a80db
Chris Zhang <chaoye917@gmail.com>
2017-07-14 00:26:46 -0700

    Simplify Title component

--- client/common/components/Title/Title.jsx
+++ client/common/components/Title/Title.jsx
@@ -3,9 +3,9 @@ import { FONT_COLOR_PRIMARY, FONT_COLOR_SECONDARY } from 'app/css/colors';
 
 const StyledTitle = styled.h1`
   font-weight: 400;
+  margin: 10px 0;
   font-size: 2rem;
-  margin: ${props => (props.resetMargins ? '0' : '10px 0')};
-  color: ${props => (props.dark ? FONT_COLOR_SECONDARY : FONT_COLOR_PRIMARY)};
+  color: ${props => (props.secondary ? FONT_COLOR_SECONDARY : FONT_COLOR_PRIMARY)};
 `;
 
 export default StyledTitle;
