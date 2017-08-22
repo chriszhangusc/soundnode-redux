7821d4e8869b09f0fe249020fbaa87ba7138b540
Chris Zhang <chaoye917@gmail.com>
2017-08-22 10:29:53 -0700

    Fix font color issue for player time section

--- client/features/player/PlayerTimeSection/Time.jsx
+++ client/features/player/PlayerTimeSection/Time.jsx
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export default styled.span`
   font-size: 0.9rem;
-  color: ${props => props.theme.fontColorSub}
+  color: ${props => props.theme.colors.fontColorSub};
   margin-right: 10px;
 `;
