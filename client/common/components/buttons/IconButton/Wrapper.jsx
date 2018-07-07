8dfede58569c2b47c9401bc453192afad6e771c9
Mini P.E.K.K.A <chaoye917@gmail.com>
2018-07-06 22:11:46 -0700

    chore(Button): add hack comment

--- client/common/components/buttons/IconButton/Wrapper.jsx
+++ client/common/components/buttons/IconButton/Wrapper.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export default styled.button`
   padding: 4px;
   border: none;
+  /* HACK: This is a hack to fix the ugly outline set by bootstrap */
   outline: none !important;
   background-color: transparent;
   cursor: ${props => !props.disabled && 'pointer'};
