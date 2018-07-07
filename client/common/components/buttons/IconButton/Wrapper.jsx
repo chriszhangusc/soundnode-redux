be526745a6a2df3745510a5bdd1ea8f865be1604
Mini P.E.K.K.A <chaoye917@gmail.com>
2018-07-06 22:11:00 -0700

    fix(Button): fix the ugly outline styling

--- client/common/components/buttons/IconButton/Wrapper.jsx
+++ client/common/components/buttons/IconButton/Wrapper.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export default styled.button`
   padding: 4px;
   border: none;
-  outline: none;
+  outline: none !important;
   background-color: transparent;
   cursor: ${props => !props.disabled && 'pointer'};
   text-align: center;
