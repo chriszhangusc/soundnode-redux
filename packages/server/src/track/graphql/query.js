b7ec35391029524517fab48baf5a5e0a6efd35b5
Mini P.E.K.K.A <chaoye917@gmail.com>
2019-01-06 23:00:09 -0800

    use direnv to load CLIENT_ID

--- packages/server/src/track/graphql/query.js
+++ packages/server/src/track/graphql/query.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const { GraphQLNonNull, GraphQLInt } = require('graphql');
-const { BASE_V1, CLIENT_ID } = require('@soundnode-redux/server/src/consts');
+const { BASE_V1 } = require('@soundnode-redux/server/src/consts');
 const { Track } = require('./type');
 
 module.exports = {
@@ -12,7 +12,9 @@ module.exports = {
       },
     },
     resolve(parentValue, args) {
-      return axios.get(`${BASE_V1}/tracks/${args.id}?client_id=${CLIENT_ID}`).then(res => res.data);
+      return axios
+        .get(`${BASE_V1}/tracks/${args.id}?client_id=${process.env.CLIENT_ID}`)
+        .then(res => res.data);
     },
   },
 };
