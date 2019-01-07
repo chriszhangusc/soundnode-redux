e6173687d783eb40f376b3a128a54ddbe91840dd
Mini P.E.K.K.A <chaoye917@gmail.com>
2019-01-06 21:11:42 -0800

    clean up

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -15,15 +15,6 @@ module.exports = {
   rules: {
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'import/no-unresolved': 0,
-    // "class-methods-use-this": 0,
-    // "import/extensions": 0,
-    // "import/no-extraneous-dependencies": 0,
-    // "react/no-array-index-key": 0,
-    // "jsx-a11y/no-static-element-interactions": 0,
-    // "function-paren-newline": 0,
-    // "prefer-destructuring": 0,
-    // "new-cap": 0,
+    'import/no-unresolved': 'off',
   },
 };
