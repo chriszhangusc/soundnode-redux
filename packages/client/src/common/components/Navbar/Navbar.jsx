253b5b1b9eb8a3c98c32fd3f8a405210c3bf1cb2
Mini P.E.K.K.A <chaoye917@gmail.com>
2019-01-16 20:45:48 -0800

    remove grid-styled

--- packages/client/src/common/components/Navbar/Navbar.jsx
+++ packages/client/src/common/components/Navbar/Navbar.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
 import SearchSuggestion from '@soundnode-redux/client/src/features/searchSuggestion/SearchSuggestion';
 import Auth from '@soundnode-redux/client/src/features/auth/Auth';
-import { Flex } from 'grid-styled';
 import BoxShadow from '@soundnode-redux/client/src/common/components/BoxShadow';
 import SidebarToggleButton from './SidebarToggleButton';
 import NavBrand from './NavBrand';
 import NavWrapper from './NavWrapper';
 
+const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 87px;
+`;
+
+const SidebarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex: 1;
+`;
+
+const SuggestionWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex: 1;
+`;
+
 function Navbar() {
   return (
     <NavWrapper>
       <BoxShadow blur={10} spread={8} shade={3}>
-        <Flex justify="space-between" px={87} py={0}>
-          <Flex align="center" justify="flex-start" flex="1">
+        <ContentWrapper>
+          <SidebarWrapper>
             <SidebarToggleButton />
             <NavBrand to="/">SoundNode Redux</NavBrand>
-          </Flex>
+          </SidebarWrapper>
 
-          <Flex align="center" justify="flex-end" flex="1">
+          <SuggestionWrapper>
             <SearchSuggestion />
             <Auth />
-          </Flex>
-        </Flex>
+          </SuggestionWrapper>
+        </ContentWrapper>
       </BoxShadow>
     </NavWrapper>
   );
