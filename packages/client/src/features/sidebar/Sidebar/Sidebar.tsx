3b3ed35bc63e646aa8faeb8d91431af38c5ac04d
Mini P.E.K.K.A <chaoye917@gmail.com>
2019-01-21 20:21:51 -0800

    Sidebar to typescript, add styled components types

--- packages/client/src/features/sidebar/Sidebar/Sidebar.tsx
+++ packages/client/src/features/sidebar/Sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import * as routes from '@soundnode-redux/client/src/common/constants/routeConsts';
 import BoxShadow from '@soundnode-redux/client/src/common/components/BoxShadow';
+import Fixed from '@soundnode-redux/client/src/common/components/Fixed';
 import PropTypes from 'prop-types';
 import { hideSidebar } from '@soundnode-redux/client/src/features/sidebar/sidebarActions';
 import SidebarHeader from '@soundnode-redux/client/src/features/sidebar/SidebarHeader';
@@ -9,9 +11,8 @@ import SidebarOverlay from '@soundnode-redux/client/src/features/sidebar/Sidebar
 import { isSidebarHidden } from '@soundnode-redux/client/src/features/sidebar/sidebarSelectors';
 import SidebarTab from '@soundnode-redux/client/src/features/sidebar/SidebarTab';
 import { withRouter } from 'react-router';
-import Wrapper from './Wrapper';
 
-const sidebarItemList = [
+const SIDEBAR_ITEM_LIST = [
   {
     to: routes.CHARTS_ROUTE,
     iconName: 'trophy',
@@ -34,8 +35,27 @@ const sidebarItemList = [
   },
 ];
 
-function Sidebar(props) {
+const Wrapper = styled(Fixed)`
+  width: 300px;
+  top: 0;
+  left: 0;
+  height: 100%;
+  background-color: ${props => props.theme.colors.bgSub};
+  z-index: ${props => props.theme.zIndexes.sidebar};
+  transform: translateX(
+    ${props => (props.sidebarHidden ? '-310px' : '0')}
+  ); /* 10px more to hide shadow */
+  transition: transform 400ms ease-in-out;
+`;
+
+type Props = {
+  hidden: boolean;
+  hideSidebar: () => void;
+};
+
+function Sidebar(props: Props) {
   const { hidden } = props;
+
   return (
     <div>
       <SidebarOverlay active={!hidden} onClick={props.hideSidebar} />
@@ -43,7 +63,7 @@ function Sidebar(props) {
         <BoxShadow offsetX={2} offsetY={2} blur={10} spread={4} shade={9}>
           <SidebarHeader />
           <ul>
-            {sidebarItemList.map(item => (
+            {SIDEBAR_ITEM_LIST.map(item => (
               <SidebarTab {...item} key={item.title} onClick={props.hideSidebar} />
             ))}
           </ul>
@@ -52,11 +72,6 @@ function Sidebar(props) {
     </div>
   );
 }
-
-Sidebar.propTypes = {
-  hidden: PropTypes.bool.isRequired,
-  hideSidebar: PropTypes.func.isRequired,
-};
 
 function mapStateToProps(state) {
   return {
@@ -69,4 +84,9 @@ Why use withRouter here?
 There will be problem when using connect from redux
 because connect will block update of route change.
 */
-export default withRouter(connect(mapStateToProps, { hideSidebar })(Sidebar));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { hideSidebar },
+  )(Sidebar),
+);
