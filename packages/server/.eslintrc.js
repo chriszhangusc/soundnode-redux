81f004e6c7c3731dbb8567c284f229c34d333846
Mini P.E.K.K.A <chaoye917@gmail.com>
2019-02-17 11:16:57 -0800

    update eslint for server

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -1,12 +1,33 @@
 module.exports = {
-  extends: ['eslint:recommended', 'airbnb-base'],
-  plugins: ['import'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'prettier',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  env: {
+    node: true,
+  },
   rules: {
-    'arrow-body-style': 'off',
-    'arrow-parens': 'off',
-    'prefer-destructuring': 'off',
-    'no-underscore-dangle': 'off',
-    'import/no-extraneous-dependencies': 'off',
+    'prettier/prettier': 'error',
     'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
   },
 };
